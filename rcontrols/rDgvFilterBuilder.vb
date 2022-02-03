b770f203a801774371949a00882e0e40e58d5a2d
derykroot <derykroot@gmail.com>
2022-02-03 08:45:24 -0300

    Filters Implemented

---
+++ rcontrols/rDgvFilterBuilder.vb
@@ -0,0 +1,75 @@
+﻿Public Class rDgvFilterBuilder
+    Public Enum FilterType
+        Igual = 0
+        Maior = 1
+        Menor = 2
+        Inicio = 3
+        Contem = 4
+    End Enum
+    Public Class ItemColFilter
+        Public Property colidx As Integer
+        Public Property Ftype As FilterType
+        Public Property Vlr As String
+    End Class
+
+    Private rDGV As rDataGridView = Nothing
+    Private Itens As New List(Of ItemColFilter)
+
+    Private chfilter As Char = "⧨"
+
+    Public Sub New(ByRef _dgv As rDataGridView)
+        rDGV = _dgv
+    End Sub
+
+
+    Public Sub Add(itf As ItemColFilter)
+        Itens.Add(itf)
+        CompileFilter()
+    End Sub
+
+    Public Sub Remove(colidx As Integer)
+        rDGV.Columns(colidx).HeaderText = rDGV.Columns(colidx).HeaderText.Replace(chfilter, "")
+        'Itens.Remove(Itens.Find(Function(it) it.colidx = colidx))
+        Itens.FindAll(Function(it) it.colidx = colidx).ForEach(Sub(r) Itens.Remove(r))
+        CompileFilter()
+    End Sub
+    Public Sub RemoveAll()
+        Itens.Clear()
+        For Each c As DataGridViewColumn In rDGV.Columns
+            c.HeaderText = c.HeaderText.Replace(chfilter, "")
+        Next
+        CompileFilter()
+    End Sub
+
+    Public Function containsfilter(colidx As Integer) As Boolean
+        Return Itens.Exists(Function(it) it.colidx = colidx)
+    End Function
+
+    Public ReadOnly Property Count() As Integer
+        Get
+            Return Itens.Count
+        End Get
+    End Property
+
+
+
+    Private Sub CompileFilter()
+        Dim stbuild As String = ""
+
+        Itens.ForEach(Sub(it)
+                          Dim nmcol = rDGV.DataView.Table.Columns(it.colidx).ColumnName
+                          If Len(stbuild) > 0 Then stbuild += " and "
+                          Select Case it.Ftype
+                              Case FilterType.Igual : stbuild += nmcol & " = " & it.Vlr
+                              Case FilterType.Maior : stbuild += nmcol & " > " & it.Vlr
+                              Case FilterType.Menor : stbuild += nmcol & " < " & it.Vlr
+                              Case FilterType.Inicio : stbuild += nmcol & " LIKE '" & it.Vlr & "%'"
+                              Case FilterType.Contem : stbuild += nmcol & " LIKE '%" & it.Vlr & "%'"
+                          End Select
+                          rDGV.Columns(it.colidx).HeaderText += If(rDGV.Columns(it.colidx).HeaderText.Contains(chfilter), "", " " & chfilter)
+                      End Sub)
+
+        rDGV.DataView.RowFilter = stbuild
+    End Sub
+
+End Class
--- rcontrols/rDgvMenuColumn.vb
+++ rcontrols/rDgvMenuColumn.vb
@@ -1,20 +1,36 @@
 ﻿Public Class rDgvMenuColumn
     Private mPopup As New ToolStripDropDown()
-    Private rDGV As DataGridView = Nothing
+    Private rDGV As rDataGridView = Nothing
 
     Private TSMFreeze As New ToolStripButton()
+    Private TSMFilterAdd As New ToolStripButton()
+
+    Private TSMFilterDel As New ToolStripButton() ' Remover Filtros da coluna
+    Private TSMFilterClear As New ToolStripButton() ' Remover todos filtros da Tabela
 
     Private colclickidx As Integer
     Private rowclickidx As Integer
 
+    Private filterbd As rDgvFilterBuilder
+
     Public Event ColFrozenChangebyUser(ByVal colidx As Integer)
-    Public Sub New(dgv As DataGridView)
+    Public Sub New(dgv As rDataGridView)
         rDGV = dgv
+        filterbd = New rDgvFilterBuilder(rDGV)
 
         mPopup.Items.Add(TSMFreeze)
+        mPopup.Items.Add(TSMFilterAdd)
+        mPopup.Items.Add(TSMFilterDel)
+        mPopup.Items.Add(TSMFilterClear)
+
+        TSMFilterDel.Text = "Remove Filter(s)"
+        TSMFilterClear.Text = "Clear All Filters"
 
         AddHandler rDGV.CellMouseDown, AddressOf DGV_CellMouseDown
         AddHandler TSMFreeze.Click, AddressOf TSMFreeze_Click
+        AddHandler TSMFilterAdd.Click, AddressOf TSMFilterAdd_Click
+        AddHandler TSMFilterDel.Click, Sub() filterbd.Remove(colclickidx)
+        AddHandler TSMFilterClear.Click, Sub() filterbd.RemoveAll()
 
     End Sub
 
@@ -27,6 +43,11 @@
 
             Dim colfrozen As Integer = (From cl As DataGridViewColumn In rDGV.Columns).Where(Function(c) c.Frozen).Select(Of Integer)(Function(cl) cl.Index).LastOrDefault(Function(b) -2)
             TSMFreeze.Text = If(colfrozen = colclickidx, "Un", "") & "Freeze Column"
+            TSMFilterAdd.Text = "Add Filter"
+
+            TSMFilterDel.Visible = filterbd.containsfilter(colclickidx)
+            TSMFilterClear.Visible = filterbd.Count > 0
+
             mPopup.Show(System.Windows.Forms.Cursor.Position)
         End If
     End Sub
@@ -37,10 +58,102 @@
         SetFreeze(idx)
         RaiseEvent ColFrozenChangebyUser(idx)
     End Sub
+    Private Sub TSMFilterAdd_Click(sender As Object, e As EventArgs)
+        Dim mpop As New ToolStripDropDown()
+        Dim pn = New pncond(filterbd, colclickidx, (rDGV.DataView.Table.Columns(colclickidx).DataType.Name <> "String"))
+        mpop.Items.Add(New ToolStripControlHost(pn))
+        mpop.Show(Cursor.Position)
+        pn.initfocus()
+    End Sub
 
     Public Sub SetFreeze(colidx As Integer)
         For Each c As DataGridViewColumn In rDGV.Columns
             c.Frozen = (colidx = c.Index)
         Next
     End Sub
+
+    Private Class pncond
+        Inherits Panel
+
+        Private lbcond As New Label()
+        Private WithEvents cbcond As New ComboBox()
+
+        Private lbvlr As New Label()
+        Private WithEvents txvlr As New TextBox()
+
+        Private fbuilder As rDgvFilterBuilder
+        Private isnumber As Boolean
+        Private colidx As Integer
+
+        Public Sub New(ByRef _fbuilder As rDgvFilterBuilder, _colidx As Integer, Optional _isnumber As Boolean = True)
+            fbuilder = _fbuilder
+            isnumber = _isnumber
+            colidx = _colidx
+            Me.Size = New Size(200, 200)
+
+            lbcond.Text = "Cond: "
+            lbcond.Size = New Size(40, 20)
+
+            cbcond.Items.AddRange(If(isnumber, {"Igual", "Maior", "Menor"}, {"Inicio", "Contém"}))
+            cbcond.Location = New Point(lbcond.Size.Width + 10, lbcond.Location.Y)
+            cbcond.SelectedIndex = 0
+            cbcond.DropDownStyle = ComboBoxStyle.DropDownList
+
+            lbvlr.Text = "Valor: "
+            lbvlr.Size = New Size(40, 20)
+            lbvlr.Location = New Point(lbcond.Location.X, lbcond.Size.Height + lbcond.Location.Y + 5)
+
+            txvlr.Location = New Point(lbvlr.Size.Width + 10, lbvlr.Location.Y)
+            txvlr.Size = cbcond.Size
+
+            Me.Controls.Add(lbcond)
+            Me.Controls.Add(cbcond)
+            Me.Controls.Add(lbvlr)
+            Me.Controls.Add(txvlr)
+        End Sub
+
+        Private Sub cbcond_KeyDown(sender As Object, e As KeyEventArgs) Handles cbcond.KeyDown
+            If e.KeyCode = Keys.Return Or e.KeyCode = Keys.Tab Then
+                txvlr.Focus()
+            ElseIf e.KeyCode = Keys.Escape Then
+                CType(Me.Parent, ToolStripDropDown).Close()
+            End If
+        End Sub
+        Private Sub txvlr_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txvlr.KeyPress
+            If Asc(e.KeyChar) = 13 Then
+                SetFilter()
+            ElseIf Asc(e.KeyChar) = 27 Then
+                CType(Me.Parent, ToolStripDropDown).Close()
+            End If
+        End Sub
+
+        Private Sub SetFilter()
+            If isnumber And Not IsNumeric(txvlr.Text) Then MsgBox("Insira um numero válido!") : txvlr.Focus() : Exit Sub
+
+            Dim tp As rDgvFilterBuilder.FilterType
+
+            If isnumber Then
+                Select Case cbcond.SelectedIndex
+                    Case 0 : tp = rDgvFilterBuilder.FilterType.Igual
+                    Case 1 : tp = rDgvFilterBuilder.FilterType.Maior
+                    Case 2 : tp = rDgvFilterBuilder.FilterType.Menor
+                End Select
+            Else
+                Select Case cbcond.SelectedIndex
+                    Case 0 : tp = rDgvFilterBuilder.FilterType.Inicio
+                    Case 1 : tp = rDgvFilterBuilder.FilterType.Contem
+                End Select
+            End If
+
+            Dim vl = If(isnumber, txvlr.Text.Replace(",", "."), txvlr.Text)
+
+            fbuilder.Add(New rDgvFilterBuilder.ItemColFilter() With {.colidx = colidx, .Ftype = tp, .Vlr = vl})
+            CType(Me.Parent, ToolStripDropDown).Close()
+        End Sub
+        Public Sub initfocus()
+            cbcond.Focus()
+        End Sub
+
+
+    End Class
 End Class
